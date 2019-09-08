32646a97065a638fce594e9603acabb06d74a124
Roopa <roopatel@valdosta.edu>
2019-09-07 22:09:25 -0400

    controllers with handlebars, devoured not working

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,42 @@
+// var express = require("express");
+// var router = express.Router();
+// var burgers = require("../models/burger.js");
+
+// router.get("/", function(req,res){
+//     burgers.selectAll(function(data){
+// 		var hbsObject = {
+// 			burgers: data
+// 		};
+// 		console.log(hbsObject);
+// 		res.render("index", hbsObject);
+// 	});
+// });
+
+// router.post("/burgers/create", function(req,res){
+// 	burgers.insertOne(["burger_name"], [req.body.burger_name], function(result) {
+//         // res.json({ id: result.insertId });
+//         res.render("index", result);
+//     // res.redirect("/burgers");
+// 	});
+// });
+
+// router.put("/burgers/update/:id", function(req,res){
+// 	var condition = "id = " + req.params.id;
+// 	console.log("condition", condition);
+
+// 	burgers.updateOne({
+// 		"devoured": req.body.devoured
+// 	}, condition, function(data){
+// 		res.redirect("/burgers")
+// 	});
+// });
+
+// module.exports = router;
+
 var express = require("express");
+
 var router = express.Router();
+
 var burgers = require("../models/burger.js");
 
 router.get("/", function(req,res){
@@ -19,10 +56,10 @@ router.get("/burgers", function(req,res){
 router.post("/burgers/create", function(req,res){
 	burgers.insertOne([
 		"burger_name"
-	],[
-		req.body.burger_name
-	], function(data){
-    	res.redirect("/burgers");
+		],[
+			req.body.burger_name
+			], function(data){
+				res.redirect("/burgers");
 			});
 });
 
